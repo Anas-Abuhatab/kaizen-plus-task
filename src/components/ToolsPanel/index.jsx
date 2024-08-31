375611f8d131111d0904ce4f4bbb837f69d5ac66
Anas-Abuhatab <anasm.abuhatab@gmail.com>
2024-08-31 23:40:51 +0300

    finish

--- src/components/ToolsPanel/index.jsx
+++ src/components/ToolsPanel/index.jsx
@@ -1,18 +1,34 @@
 import "@/styles/components/toolsPanel.scss";
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 const ToolsPanel = (props) => {
   const [show, setShow] = useState(true);
-  const [showEvent, setShowEvent] = useState(true);
+  const [showEvent, setShowEvent] = useState(false);
+  const [data, setData] = useState({
+    currentLayout: null,
+  });
+
   const navigate = useNavigate();
   const handleNavigate = (url) => {
     navigate(url);
   };
   const handleShowDetails = () => {
     setShowEvent(!showEvent);
+  };
+  const showLayoutSelector = (layout) => {
+    data.currentLayout = layout;
+    props.setMapLayout(layout);
+    setData({ ...data });
+    setShowEvent(false);
   };
   return (
     <div className="position-relative">
@@ -51,10 +67,10 @@ const ToolsPanel = (props) => {
                   handleShowDetails();
                 }}
                 variant="text"
-                className="d-flex flex-column align-items-center tools-panel__btn"
+                className={`d-flex flex-column align-items-center tools-panel__btn ${!showEvent ? "tools-panel__btn--event-btn" : ""}`}
               >
                 <img src="/assets/events.png" alt="home.png" width={25} />
-                <p className="mt-2">Events</p>
+                <p className="mt-2" style={{color : showEvent ? "#1186FA" :"var(--text)"}}>Events</p>
               </Button>
               <Button
                 onClick={() => handleNavigate("/map")}
@@ -94,16 +110,20 @@ const ToolsPanel = (props) => {
 
       {show && showEvent && (
         <div className="details-popup">
-          <div>
+          <div className="d-flex justify-content-start align-items-center w-100 p-2">
             <InfoOutlinedIcon
-              style={{ cursor: "help", marginLeft: "auto" }}
+              className="mx-2"
+              style={{ cursor: "help" }}
               titleAccess={props.info}
               color="inherit"
             />
-            <p>Emergency Details</p>
+            <p className="fw-bold">Emergency Details</p>
           </div>
-          <div>
+          <div className="my-3">
+            <p className="my-2 fw-bold">Emergency Domain</p>
             <TextField
+              className="w-100 rounded mb-3"
+              style={{ background: "#2C2C2C" }}
               placeholder="Refining - RR1"
               id="emergency_domain"
               name="emergency_domain"
@@ -113,6 +133,115 @@ const ToolsPanel = (props) => {
                 shrink: true,
               }}
             />
+          </div>
+          <Divider sx={{ borderWidth: "1px", borderColor: "#fff" }} />
+
+          <div className="my-3">
+            <p className="my-2 fw-bold">Emergency Area</p>
+            <p className="my-2 fw-bold">Choose emergency area shape</p>
+            <div className="d-flex justify-content-start align-items-center w-100 p-2 gap-2 overflow-auto">
+              <Button
+                onClick={() => {
+                  showLayoutSelector("RECTANGLE");
+                }}
+                variant="text"
+              >
+                <img width={40} src="/assets/Frame1.png" alt="frame" />
+              </Button>
+              <Button
+                onClick={() => {
+                  showLayoutSelector("CIRCLE");
+                }}
+                variant="contained"
+              >
+                <img width={40} src="/assets/Frame2.png" alt="frame" />
+              </Button>
+              <Button
+                onClick={() => {
+                  showLayoutSelector("POLYGON");
+                }}
+                variant="contained"
+              >
+                <img width={40} src="/assets/Frame3.png" alt="frame" />
+              </Button>
+              <Button
+                onClick={() => {
+                  showLayoutSelector("POLYGON");
+                }}
+                variant="contained"
+              >
+                <img width={40} src="/assets/Frame4.png" alt="frame" />
+              </Button>
+            </div>
+          </div>
+          <Divider sx={{ borderWidth: "1px", borderColor: "#fff" }} />
+
+          <div className="my-3">
+            <p className="my-2 fw-bold">Emergency Name</p>
+            <TextField
+              className="w-100 rounded mb-3"
+              style={{ background: "#2C2C2C" }}
+              placeholder="Default Name"
+              id="emergency_domain"
+              name="emergency_domain"
+              type="text"
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </div>
+          <Divider sx={{ borderWidth: "1px", borderColor: "#fff" }} />
+
+          <div className="my-3">
+            <p className="my-2 fw-bold">Emergency Layers</p>
+            <div className="d-flex flex-column justify-content-start align-items-start">
+              <FormControlLabel
+                className="mx-0"
+                control={<Checkbox color="info" />}
+                label={"Employees"}
+              />
+              <FormControlLabel
+                className="mx-0"
+                control={<Checkbox color="info" />}
+                label={"Points of interest"}
+              />
+              <FormControlLabel
+                className="mx-0"
+                control={<Checkbox color="info" />}
+                label={"HSE Zones"}
+              />
+              <FormControlLabel
+                className="mx-0"
+                control={<Checkbox color="info" />}
+                label={"Events"}
+              />
+            </div>
+            <div className="d-flex justify-content-center align-items-center w-100 gap-2 flex-xs-column flex-md-row">
+              <Button
+                style={{ backgroundColor: "#404040" }}
+                onClick={() => {
+                  setShowEvent(false);
+                }}
+                variant="contained"
+                color="inherit"
+                className="rounded"
+                sx={{ fontSize: ".7rem" }}
+              >
+                Save & Broadcast later
+              </Button>
+              <Button
+                onClick={() => {
+                  setShowEvent(false);
+                }}
+                variant="contained"
+                color="info"
+                className="rounded"
+                sx={{ fontSize: ".7rem" }}
+              >
+                Save & Broadcast Now
+              </Button>
+            </div>
           </div>
         </div>
       )}
--- src/routes/Map/index.jsx
+++ src/routes/Map/index.jsx
@@ -7,24 +7,50 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 const TheMap = (props) => {
   const [map, setMap] = useState(null); // State to store the map instance
   const [showList, setShowList] = useState(null);
+  const [mapLayout, setMapLayout] = useState(null);
+  const drawingManagerRef = useRef(null);
+  const [markers, setMarkers] = useState([]); // Store markers
 
   const mapRef = useRef(null);
+
+  const addMarker =async (location) => {
+
+    const { Marker } = await window.google.maps.importLibrary("marker");
+
+    const marker = new Marker({
+      position: location,
+      map: map,
+      draggable: true, // Allow the marker to be dragged
+    });
+    // Store the marker in state
+    setMarkers((prevMarkers) => [...prevMarkers, marker]);
+
+    // Handle marker drag event
+    marker.addListener("dragend", (event) => {
+      const newPosition = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      };
+    });
+  };
 
   useEffect(() => {
     const loader = new Loader({
       apiKey: "AIzaSyDEjGbNU2as8Rs0uMqngzYUYnxZLCqTL7s",
       version: "weekly",
-      libraries: ["places"],
+      libraries: ["places", "drawing"],
     });
     loader.load().then(async () => {
       if (mapRef.current) {
-        const { Map } = await window.google.maps.importLibrary("maps");
+        const { Map, DrawingManager } = await window.google.maps.importLibrary(
+          "maps"
+        );
 
         const mapInstance = new Map(mapRef.current, {
           mapId: "ddddd" + Math.random(),
           center: {
-            lat: 24.718611,
-            lng: 46.676044,
+            lat: 31.9544,
+            lng: 35.9106,
           },
           zoom: 12, // Initial zoom level
           streetViewControl: false, // Disable Street View (Pegman)
@@ -37,22 +63,20 @@ const TheMap = (props) => {
 
         const marker = new Marker({
           position: {
-            lat: 24.718611,
-            lng: 46.676044,
+            lat: 31.9544,
+            lng: 35.9106,
           },
           map: mapInstance,
-          draggable: props.EditMode, // Allow the marker to be dragged
+          draggable: true, // Allow the marker to be dragged
         });
 
-        if (props.EditMode) {
+        
           marker?.addListener("dragend", (event) => {
             const newLat = event.latLng.lat();
             const newLng = event.latLng.lng();
 
             // Smoothly pan the map to the new marker position
             mapInstance.panTo({ lat: newLat, lng: newLng });
-
-            props.getMapPosition({ lat: newLat, lng: newLng });
           });
 
           // Add an event listener for clicks on the map
@@ -60,15 +84,98 @@ const TheMap = (props) => {
             const newLat = event.latLng.lat();
             const newLng = event.latLng.lng();
 
+            addMarker({ lat: newLat, lng: newLng })
             // Move the marker to the new position
-            marker.setPosition({ lat: newLat, lng: newLng });
+            // marker.setPosition({ lat: newLat, lng: newLng });
 
             // Smoothly pan the map to the new marker position
             mapInstance.panTo({ lat: newLat, lng: newLng });
 
-            props.getMapPosition({ lat: newLat, lng: newLng });
           });
-        }
+        
+        const drawingManager = await new window.google.maps.drawing.DrawingManager({
+          drawingMode: null, // No mode by default; you can activate one by default if needed
+          drawingControl: true, // Show drawing controls
+          drawingControlOptions: {
+            position: window.google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: ["rectangle", "circle", "polygon"], // Shapes you want to allow
+          },
+          rectangleOptions: {
+            fillColor: "#ff0000",
+            fillOpacity: 0.2,
+            strokeWeight: 2,
+            clickable: false,
+            editable: true,
+            zIndex: 1,
+          },
+          circleOptions: {
+            fillColor: "#ff0000",
+            fillOpacity: 0.2,
+            strokeWeight: 2,
+            clickable: false,
+            editable: true,
+            zIndex: 1,
+          },
+          polygonOptions: {
+            fillColor: "#ff0000",
+            fillOpacity: 0.2,
+            strokeWeight: 2,
+            clickable: false,
+            editable: true,
+            zIndex: 1,
+          },
+        });
+        drawingManager.setMap(mapInstance);
+        drawingManagerRef.current = drawingManager;
+
+        window.google.maps.event.addListener(
+          drawingManager,
+          "overlaycomplete",
+          (event) => {
+            const shape = event.overlay;
+            shape.type = event.type;
+
+            if (shape.type === "rectangle") {
+              const bounds = shape.getBounds();
+              const ne = bounds.getNorthEast();
+              const sw = bounds.getSouthWest();
+
+              let result = {
+                type: "rectangle",
+                coordinates: {
+                  northEast: { lat: ne.lat(), lng: ne.lng() },
+                  southWest: { lat: sw.lat(), lng: sw.lng() },
+                },
+              }
+              // console.log(result);
+              
+            } else if (shape.type === "circle") {
+              const center = shape.getCenter();
+              const radius = shape.getRadius();
+
+              let result = {
+                type: "circle",
+                coordinates: {
+                  center: { lat: center.lat(), lng: center.lng() },
+                  radius,
+                },
+              }
+              // console.log(result);
+            } else if (shape.type === "polygon") {
+              const path = shape.getPath();
+              const coordinates = [];
+              path.forEach((point) => {
+                coordinates.push({ lat: point.lat(), lng: point.lng() });
+              });
+
+              let result = {
+                type: "polygon",
+                coordinates,
+              }
+              // console.log(result);
+            }
+          }
+        );
       }
     });
   }, []);
@@ -80,100 +187,147 @@ const TheMap = (props) => {
     setShowList(false);
     switch (type) {
       case "Assembly Point":
-        
         break;
-    
+
       default:
         break;
     }
     setShowList(!showList);
   };
 
+  const handleSetMapLayout = (layout) => {
+    setMapLayout(layout);
+  };
+
+  const handleDrawRectangle = (mode) => {
+    if (drawingManagerRef.current) {
+      drawingManagerRef.current.setDrawingMode(window.google.maps.drawing.OverlayType[`${mode}`]);
+    }
+  };
+  useEffect(() => {
+    handleDrawRectangle(mapLayout)
+  }, [mapLayout]);
+
   return (
-    <div className="the-map">
-      <ToolsPanel class="position-absolute z-2" />
-      <div ref={mapRef} style={{ width: "100%", height: "100vh" }} />
-      <button onClick={handleShowAddList} className="map-add-btn">
-        <AddCircleIcon fontSize="inherit" color="inherit" />
-      </button>
-      {showList && (
-        <div className="map-add-list">
-          <div>
-            <p className="fs-6 fw-bold text-light p-3">
-              Add Points of Interests
-            </p>
-            <ul className="map-add-list__list border-top p-3">
-              <li onClick={()=>{handleSelect("Assembly Point")}} className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center">
-                <span className="mx-2">
-                  <img
-                    src="/assets/icon8.png"
-                    alt="map-icon"
-                    width={20}
-                    height={20}
-                  />
-                </span>
-                <span className="text-light mx-2">Assembly Point</span>
-              </li>
-              <li onClick={()=>{handleSelect("Fire Extinguisher")}} className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center">
-                <span className="mx-2">
-                  <img
-                    src="/assets/icon5.png"
-                    alt="map-icon"
-                    width={20}
-                    height={20}
-                  />
-                </span>
-                <span className="text-light mx-2">Fire Extinguisher</span>
-              </li>
-              <li onClick={()=>{handleSelect("Safety Exit")}} className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center">
-                <span className="mx-2">
-                  <img
-                    src="/assets/icon7.png"
-                    alt="map-icon"
-                    width={20}
-                    height={20}
-                  />
-                </span>
-                <span className="text-light mx-2">Safety Exit</span>
-              </li>
-              <li onClick={()=>{handleSelect("First Aid")}} className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center">
-                <span className="mx-2">
-                  <img
-                    src="/assets/icon6.png"
-                    alt="map-icon"
-                    width={20}
-                    height={20}
-                  />
-                </span>
-                <span className="text-light mx-2">First Aid</span>
-              </li>
-              <li onClick={()=>{handleSelect("Shower")}} className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center">
-                <span className="mx-2">
-                  <img
-                    src="/assets/icon10.png"
-                    alt="map-icon"
-                    width={20}
-                    height={20}
-                  />
-                </span>
-                <span className="text-light mx-2">Shower</span>
-              </li>
-              <li onClick={()=>{handleSelect("Life Boat")}} className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center">
-                <span className="mx-2">
-                  <img
-                    src="/assets/icon9.png"
-                    alt="map-icon"
-                    width={20}
-                    height={20}
-                  />
-                </span>
-                <span className="text-light mx-2">Life Boat</span>
-              </li>
-            </ul>
+    <>
+      <div className="the-map">
+        <ToolsPanel
+          class="position-absolute z-2"
+          setMapLayout={handleSetMapLayout}
+        />
+        <div ref={mapRef} style={{ width: "100%", height: "100vh" }} />
+        <button onClick={handleShowAddList} className="map-add-btn">
+          <AddCircleIcon fontSize="inherit" color="inherit" />
+        </button>
+        {showList && (
+          <div className="map-add-list">
+            <div>
+              <p className="fs-6 fw-bold text-light p-3">
+                Add Points of Interests
+              </p>
+              <ul className="map-add-list__list border-top p-3">
+                <li
+                  onClick={() => {
+                    handleSelect("Assembly Point");
+                  }}
+                  className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center"
+                >
+                  <span className="mx-2">
+                    <img
+                      src="/assets/icon8.png"
+                      alt="map-icon"
+                      width={20}
+                      height={20}
+                    />
+                  </span>
+                  <span className="text-light mx-2">Assembly Point</span>
+                </li>
+                <li
+                  onClick={() => {
+                    handleSelect("Fire Extinguisher");
+                  }}
+                  className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center"
+                >
+                  <span className="mx-2">
+                    <img
+                      src="/assets/icon5.png"
+                      alt="map-icon"
+                      width={20}
+                      height={20}
+                    />
+                  </span>
+                  <span className="text-light mx-2">Fire Extinguisher</span>
+                </li>
+                <li
+                  onClick={() => {
+                    handleSelect("Safety Exit");
+                  }}
+                  className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center"
+                >
+                  <span className="mx-2">
+                    <img
+                      src="/assets/icon7.png"
+                      alt="map-icon"
+                      width={20}
+                      height={20}
+                    />
+                  </span>
+                  <span className="text-light mx-2">Safety Exit</span>
+                </li>
+                <li
+                  onClick={() => {
+                    handleSelect("First Aid");
+                  }}
+                  className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center"
+                >
+                  <span className="mx-2">
+                    <img
+                      src="/assets/icon6.png"
+                      alt="map-icon"
+                      width={20}
+                      height={20}
+                    />
+                  </span>
+                  <span className="text-light mx-2">First Aid</span>
+                </li>
+                <li
+                  onClick={() => {
+                    handleSelect("Shower");
+                  }}
+                  className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center"
+                >
+                  <span className="mx-2">
+                    <img
+                      src="/assets/icon10.png"
+                      alt="map-icon"
+                      width={20}
+                      height={20}
+                    />
+                  </span>
+                  <span className="text-light mx-2">Shower</span>
+                </li>
+                <li
+                  onClick={() => {
+                    handleSelect("Life Boat");
+                  }}
+                  className="d-flex justify-content-start align-items-center w-100 cursor-pointer p-2 m-1 text-center"
+                >
+                  <span className="mx-2">
+                    <img
+                      src="/assets/icon9.png"
+                      alt="map-icon"
+                      width={20}
+                      height={20}
+                    />
+                  </span>
+                  <span className="text-light mx-2">Life Boat</span>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
